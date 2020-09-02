a6e4b96dec5cfa054a50cbb71b7970ba9417d796
Kiirou <trung.09tq@gmail.com>
2020-09-02 14:09:29 +0200

    added new activities

--- app/src/main/java/com/example/miyagisrezepteecke/MainActivity.java
+++ app/src/main/java/com/example/miyagisrezepteecke/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.miyagisrezepteecke;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button BtnMove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnMove = findViewById(R.id.button4);
+        BtnMove.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                moveToAddRecipe();
+            }
+        });
+}
+    private void moveToAddRecipe() {
+        Intent intent = new Intent(MainActivity.this, AddRecipe.class);
+        startActivity(intent);
     }
 }
