46e73cf9f2ec62f37920fee6dda44092d837e33f
Kiirou <trung.09tq@gmail.com>
2020-09-03 21:50:15 +0200

    added button functionality

--- app/src/main/java/com/example/miyagisrezepteecke/MainActivity.java
+++ app/src/main/java/com/example/miyagisrezepteecke/MainActivity.java
@@ -8,32 +8,37 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button BtnMove;
+    public Button button4;
+    public Button button2;
+
+    public void btn4() {
+        button4 = (Button)findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent but4 = new Intent(MainActivity.this, AddRecipe.class);
+                startActivity(but4);
+            }
+        });
+    }
+
+    public void btn2() {
+        button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent but2 = new Intent(MainActivity.this, Einkaufsliste.class);
+                startActivity(but2);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        BtnMove = findViewById(R.id.button4);
-        BtnMove = findViewById(R.id.button2);
-        BtnMove.setOnClickListener(new View.OnClickListener(){
-
-            @Override
-            public void onClick(View v) {
-
-                moveToAddRecipe();
-                moveToEinkaufsliste();
+        btn4();
+        btn2();
             }
-        });
-}
-    private void moveToAddRecipe() {
-        Intent intent = new Intent(MainActivity.this, AddRecipe.class);
-        startActivity(intent);
-    }
 
-    private void moveToEinkaufsliste() {
-        Intent intent = new Intent(MainActivity.this, Einkaufsliste.class);
-        startActivity(intent);
-    }
 }
